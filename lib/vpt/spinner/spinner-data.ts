5be2c42e5b413f8343fed5b2b28c66b1519c7a57
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 09:12:22 -0400

    refactor(spinner): add @see link in spinner-data.ts

--- lib/vpt/spinner/spinner-data.ts
+++ lib/vpt/spinner/spinner-data.ts
@@ -6,7 +6,9 @@ import type { Storage } from '../../io/ole-doc.js'
 import { Vertex2D } from '../../math/vertex2d.js'
 import { ItemData } from '../item-data.js'
 
-/** Spinner data. */
+/** Spinner data.
+ *
+ * @see https://github.com/vpinball/vpinball/blob/master/spinner.cpp */
 export class SpinnerData extends ItemData {
 	public center!: Vertex2D
 	public rotation: number = 0
