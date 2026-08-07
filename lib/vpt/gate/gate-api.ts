de6a3df6163dd89bf40e64b4b85a83418d8f59a7
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:22:35 -0400

    refactor(gate): add docs to GateApi

--- lib/vpt/gate/gate-api.ts
+++ lib/vpt/gate/gate-api.ts
@@ -14,7 +14,8 @@ import type { GateHit } from './gate-hit.js'
 import type { GateMover } from './gate-mover.js'
 import type { GateState } from './gate-state.js'
 
-export class GateApi extends ItemApi<GateData> {
+export /** GateApi. */
+class GateApi extends ItemApi<GateData> {
 	private readonly state: GateState
 	private readonly mover: GateMover
 	private readonly hitGate: GateHit
