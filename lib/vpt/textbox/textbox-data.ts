0261795daa2aa948a72310e483c1bad453d80ebf
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 10:19:45 -0400

    refactor(textbox): simplify textbox-data — map tags

--- lib/vpt/textbox/textbox-data.ts
+++ lib/vpt/textbox/textbox-data.ts
@@ -7,33 +7,29 @@ import { Vertex2D } from '../../math/vertex2d.js'
 import { Enums } from '../enums.js'
 import { ItemData } from '../item-data.js'
 
+const FLOAT_MAP: Record<string, string> = { INSC: 'intensityScale' }
+const INT_MAP: Record<string, string> = { ALGN: 'align' }
+const BOOL_MAP: Record<string, string> = { TRNS: 'isTransparent', IDMD: 'isDMD' }
+const STRING_MAP: Record<string, string> = { TEXT: 'text' }
+
 /** Textbox data.
- *
  * @see https://github.com/vpinball/vpinball/blob/master/textbox.cpp */
 export class TextboxData extends ItemData {
 	public v1!: Vertex2D
 	public v2!: Vertex2D
-	public backColor: number = 0x000000
-	public fontColor: number = 0xffffff
-	public intensityScale: number = 1.0
-	public text: string = '0'
+	public backColor = 0x000000
+	public fontColor = 0xffffff
+	public intensityScale = 1
+	public text = '0'
 	public align: number = Enums.TextAlignment.TextAlignRight
-	public isTransparent: boolean = false
-	public isDMD: boolean = false
-
-	// non-persisted
-	public isVisible: boolean = true
+	public isTransparent = false
+	public isDMD = false
+	public isVisible = true
 
 	public static async fromStorage(storage: Storage, itemName: string): Promise<TextboxData> {
-		const textBoxData = new TextboxData(itemName)
-		await storage.streamFiltered(
-			itemName,
-			4,
-			BiffParser.stream(textBoxData.fromTag.bind(textBoxData), {
-				streamedTags: ['FONT'],
-			}),
-		)
-		return textBoxData
+		const d = new TextboxData(itemName)
+		await storage.streamFiltered(itemName, 4, BiffParser.stream(d.fromTag.bind(d), { streamedTags: ['FONT'] }))
+		return d
 	}
 
 	private constructor(itemName: string) {
@@ -41,40 +37,40 @@ export class TextboxData extends ItemData {
 	}
 
 	private async fromTag(buffer: Uint8Array, tag: string, offset: number, len: number): Promise<number> {
-		switch (tag) {
-			case 'VER1':
-				this.v1 = Vertex2D.get(buffer)
-				break
-			case 'VER2':
-				this.v2 = Vertex2D.get(buffer)
-				break
-			case 'CLRB':
-				this.backColor = BiffParser.bgrToRgb(this.getInt(buffer))
-				break
-			case 'CLRF':
-				this.fontColor = BiffParser.bgrToRgb(this.getInt(buffer))
-				break
-			case 'INSC':
-				this.intensityScale = this.getFloat(buffer)
-				break
-			case 'TEXT':
-				this.text = this.getString(buffer, len)
-				break
-			case 'ALGN':
-				this.align = this.getInt(buffer)
-				break
-			case 'TRNS':
-				this.isTransparent = this.getBool(buffer)
-				break
-			case 'IDMD':
-				this.isDMD = this.getBool(buffer)
-				break
-			case 'FONT':
-				break // ignore for now, see BiffParser#L62, it's currently treated as end of storage
-			default:
-				this.getCommonBlock(buffer, tag, len)
-				break
+		if (tag === 'VER1') {
+			this.v1 = Vertex2D.get(buffer)
+			return 0
 		}
+		if (tag === 'VER2') {
+			this.v2 = Vertex2D.get(buffer)
+			return 0
+		}
+		if (tag === 'CLRB') {
+			this.backColor = BiffParser.bgrToRgb(this.getInt(buffer))
+			return 0
+		}
+		if (tag === 'CLRF') {
+			this.fontColor = BiffParser.bgrToRgb(this.getInt(buffer))
+			return 0
+		}
+		if (tag === 'FONT') return 0
+		if (tag in FLOAT_MAP) {
+			;(this as any)[FLOAT_MAP[tag]] = this.getFloat(buffer)
+			return 0
+		}
+		if (tag in INT_MAP) {
+			;(this as any)[INT_MAP[tag]] = this.getInt(buffer)
+			return 0
+		}
+		if (tag in BOOL_MAP) {
+			;(this as any)[BOOL_MAP[tag]] = this.getBool(buffer)
+			return 0
+		}
+		if (tag in STRING_MAP) {
+			;(this as any)[STRING_MAP[tag]] = this.getString(buffer, len)
+			return 0
+		}
+		this.getCommonBlock(buffer, tag, len)
 		return 0
 	}
 }
