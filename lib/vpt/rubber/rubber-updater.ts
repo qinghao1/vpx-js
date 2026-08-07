107dbe60e7cf15310ac0f3af33dee339afbdce8e
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:31:33 -0400

    refactor(rubber): fix malformed JSDoc in rubber-updater.ts

--- lib/vpt/rubber/rubber-updater.ts
+++ lib/vpt/rubber/rubber-updater.ts
@@ -10,8 +10,8 @@ import type { Table } from '../table/table.js'
 import type { RubberData } from './rubber-data.js'
 import type { RubberState } from './rubber-state.js'
 
-export /** RubberUpdater. */
-class RubberUpdater extends ItemUpdater<RubberState> {
+/** RubberUpdater. */
+export class RubberUpdater extends ItemUpdater<RubberState> {
 	private readonly data: RubberData
 
 	private readonly middlePoint: Vertex3D
