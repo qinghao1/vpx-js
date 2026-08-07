459e5e411029c798afbde0256a3901c733922c52
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:04:05 -0400

    refactor: clean lib/emu/emulator-message-queue.spec.ts — concise dual copyright, tidy imports

--- lib/emu/emulator-message-queue.spec.ts
+++ lib/emu/emulator-message-queue.spec.ts
@@ -1,30 +1,13 @@
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+// Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
+// Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
 import * as chai from 'chai'
 import { expect } from 'chai'
 import sinonChai from 'sinon-chai'
-import type { IEmulator } from '../game/iemulator'
-import type { Vertex2D } from '../math/vertex2d'
-import { EmulatorMessageQueue, MessageType } from './emulator-message-queue'
+import type { IEmulator } from '../game/iemulator.js'
+import type { Vertex2D } from '../math/vertex2d.js'
+import { EmulatorMessageQueue, MessageType } from './emulator-message-queue.js'
 
-/* tslint:disable:no-unused-expression no-string-literal */
 chai.use((sinonChai as any).default ?? sinonChai)
 describe('The WPC-EMU message queue', () => {
 	let messageQueue: EmulatorMessageQueue
