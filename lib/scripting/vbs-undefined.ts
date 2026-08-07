d36bc2d788ef0539528ace16093a3c0e5b5439c0
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:19:28 -0400

    refactor(scripting): clean vbs-undefined — docs

--- lib/scripting/vbs-undefined.ts
+++ lib/scripting/vbs-undefined.ts
@@ -3,6 +3,7 @@
 
 import { ERR, VbsError } from './stdlib/err.js'
 
+/** VBS undefined sentinel. */
 export class VbsUndefined implements ProxyHandler<any> {
 	// tslint:disable-next-line:variable-name
 	private readonly __errSet?: VbsError
