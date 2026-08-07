8dd153d4ed187e21fbb6a60e6460339f632d38cf
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:31:31 -0400

    refactor(primitive): fix malformed JSDoc in primitive-hit-generator.ts

--- lib/vpt/primitive/primitive-hit-generator.ts
+++ lib/vpt/primitive/primitive-hit-generator.ts
@@ -22,8 +22,8 @@ import { Mesh } from '../mesh.js'
 import type { Table } from '../table/table.js'
 import type { PrimitiveData } from './primitive-data.js'
 
-export /** PrimitiveHitGenerator. */
-class PrimitiveHitGenerator {
+/** PrimitiveHitGenerator. */
+export class PrimitiveHitGenerator {
 	private readonly data: PrimitiveData
 
 	constructor(data: PrimitiveData) {
