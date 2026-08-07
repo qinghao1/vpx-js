8ea90221cb3a88cb4ff4d27d2a8d2a69b2981061
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:22:38 -0400

    refactor(hit-target): add docs to HitTargetUpdater

--- lib/vpt/hit-target/hit-target-updater.ts
+++ lib/vpt/hit-target/hit-target-updater.ts
@@ -9,7 +9,8 @@ import type { Table } from '../table/table.js'
 import type { HitTargetData } from './hit-target-data.js'
 import type { HitTargetState } from './hit-target-state.js'
 
-export class HitTargetUpdater extends ItemUpdater<HitTargetState> {
+export /** HitTargetUpdater. */
+class HitTargetUpdater extends ItemUpdater<HitTargetState> {
 	private readonly data: HitTargetData
 
 	constructor(data: HitTargetData, state: HitTargetState) {
