7d76a4656e1df6f41b450e7f07d138db906017bf
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 06:02:05 -0400

    refactor(spinner): improve generic JSDoc in spinner-state.ts

--- lib/vpt/spinner/spinner-state.ts
+++ lib/vpt/spinner/spinner-state.ts
@@ -4,7 +4,7 @@
 import { Pool } from '../../util/object-pool.js'
 import { ItemState } from '../item-state.js'
 
-/** SpinnerState. */
+/** Spinner state. */
 export class SpinnerState extends ItemState {
 	public static readonly POOL = new Pool(SpinnerState)
 
