4c67c86dc0ed75b11361a18a8dc007aa678d0e12
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 06:08:39 -0400

    refactor(ball): add getter docs in ball-api.ts

--- lib/vpt/ball/ball-api.ts
+++ lib/vpt/ball/ball-api.ts
@@ -31,24 +31,28 @@ export class BallApi extends ItemApi<BallData> {
 		this.hit = hit
 	}
 
+	/** Get X. */
 	get X() {
 		return this.state.pos.x
 	}
 	set X(v) {
 		this.state.pos.x = v
 	}
+	/** Get Y. */
 	get Y() {
 		return this.state.pos.y
 	}
 	set Y(v) {
 		this.state.pos.y = v
 	}
+	/** Get Z. */
 	get Z() {
 		return this.state.pos.z
 	}
 	set Z(v) {
 		this.state.pos.z = v
 	}
+	/** Get VelX. */
 	get VelX() {
 		return this.hit.vel.x
 	}
@@ -56,6 +60,7 @@ export class BallApi extends ItemApi<BallData> {
 		this.hit.vel.x = v
 		this.hit.calcHitBBox()
 	}
+	/** Get VelY. */
 	get VelY() {
 		return this.hit.vel.y
 	}
@@ -63,6 +68,7 @@ export class BallApi extends ItemApi<BallData> {
 		this.hit.vel.y = v
 		this.hit.calcHitBBox()
 	}
+	/** Get VelZ. */
 	get VelZ() {
 		return this.hit.vel.z
 	}
@@ -70,6 +76,7 @@ export class BallApi extends ItemApi<BallData> {
 		this.hit.vel.z = v
 		this.hit.calcHitBBox()
 	}
+	/** Get AngVelX. */
 	get AngVelX() {
 		return this.hit.angularVelocity.x
 	}
@@ -77,6 +84,7 @@ export class BallApi extends ItemApi<BallData> {
 		this.hit.angularVelocity.x = v
 		this.hit.calcHitBBox()
 	}
+	/** Get AngVelY. */
 	get AngVelY() {
 		return this.hit.angularVelocity.y
 	}
@@ -84,6 +92,7 @@ export class BallApi extends ItemApi<BallData> {
 		this.hit.angularVelocity.y = v
 		this.hit.calcHitBBox()
 	}
+	/** Get AngVelZ. */
 	get AngVelZ() {
 		return this.hit.angularVelocity.z
 	}
@@ -91,6 +100,7 @@ export class BallApi extends ItemApi<BallData> {
 		this.hit.angularVelocity.z = v
 		this.hit.calcHitBBox()
 	}
+	/** Get AngMomX. */
 	get AngMomX() {
 		return this.hit.angularMomentum.x
 	}
@@ -98,6 +108,7 @@ export class BallApi extends ItemApi<BallData> {
 		this.hit.angularMomentum.x = v
 		this.hit.calcHitBBox()
 	}
+	/** Get AngMomY. */
 	get AngMomY() {
 		return this.hit.angularMomentum.y
 	}
@@ -105,6 +116,7 @@ export class BallApi extends ItemApi<BallData> {
 		this.hit.angularMomentum.y = v
 		this.hit.calcHitBBox()
 	}
+	/** Get AngMomZ. */
 	get AngMomZ() {
 		return this.hit.angularMomentum.z
 	}
@@ -112,18 +124,21 @@ export class BallApi extends ItemApi<BallData> {
 		this.hit.angularMomentum.z = v
 		this.hit.calcHitBBox()
 	}
+	/** Get Color. */
 	get Color() {
 		return this.data.color
 	}
 	set Color(v) {
 		this.data.color = v
 	}
+	/** Get Image. */
 	get Image() {
 		return this.data.environmentMap
 	}
 	set Image(v) {
 		this.data.environmentMap = v
 	}
+	/** Get FrontDecal. */
 	get FrontDecal() {
 		return this.data.frontDecal
 	}
@@ -131,54 +146,63 @@ export class BallApi extends ItemApi<BallData> {
 		this._assertNonHdrImage(v)
 		this.data.frontDecal = v
 	}
+	/** Get DecalMode. */
 	get DecalMode() {
 		return this.data.decalMode
 	}
 	set DecalMode(v) {
 		this.data.decalMode = v
 	}
+	/** Get Mass. */
 	get Mass() {
 		return this.data.mass
 	}
 	set Mass(v) {
 		this.hit.setMass(v)
 	}
+	/** Get ID. */
 	get ID() {
 		return this.ball.id
 	}
 	set ID(v) {
 		this.ball.id = v
 	}
+	/** Get Radius. */
 	get Radius() {
 		return this.data.radius
 	}
 	set Radius(v) {
 		this.hit.setRadius(v)
 	}
+	/** Get BulbIntensityScale. */
 	get BulbIntensityScale() {
 		return this.data.bulbIntensityScale
 	}
 	set BulbIntensityScale(v) {
 		this.data.bulbIntensityScale = v
 	}
+	/** Get ReflectionEnabled. */
 	get ReflectionEnabled() {
 		return this.data.isReflectionEnabled
 	}
 	set ReflectionEnabled(v) {
 		this.data.isReflectionEnabled = v
 	}
+	/** Get PlayfieldReflectionScale. */
 	get PlayfieldReflectionScale() {
 		return this.data.playfieldReflectionStrength
 	}
 	set PlayfieldReflectionScale(v) {
 		this.data.playfieldReflectionStrength = v
 	}
+	/** Get ForceReflection. */
 	get ForceReflection() {
 		return this.data.forceReflection
 	}
 	set ForceReflection(v) {
 		this.data.forceReflection = v
 	}
+	/** Get Visible. */
 	get Visible() {
 		return this.hit.isVisible
 	}
