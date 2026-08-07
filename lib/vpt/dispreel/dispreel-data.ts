925537ad25acef4ed66d47824a9d160fa0ccab5c
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 09:12:50 -0400

    refactor(dispreel): add @see link in dispreel-data.ts

--- lib/vpt/dispreel/dispreel-data.ts
+++ lib/vpt/dispreel/dispreel-data.ts
@@ -6,7 +6,9 @@ import type { Storage } from '../../io/ole-doc.js'
 import { Vertex2D } from '../../math/vertex2d.js'
 import { ItemData } from '../item-data.js'
 
-/** DispReel data. */
+/** DispReel data.
+ *
+ * @see https://github.com/vpinball/vpinball/blob/master/dispreel.cpp */
 export class DispReelData extends ItemData {
 	public v1!: Vertex2D
 	public v2!: Vertex2D
