01b45f7d942a3cc24d8bf86932e57247cf9d9602
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:04:11 -0400

    refactor: clean lib/vpt/table/table-hit-generator.ts — concise dual copyright, tidy imports

--- lib/vpt/table/table-hit-generator.ts
+++ lib/vpt/table/table-hit-generator.ts
@@ -1,29 +1,13 @@
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+// Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
+// Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
-import { Vertex2D } from '../../math/vertex2d'
-import { Vertex3D } from '../../math/vertex3d'
-import { Hit3DPoly } from '../../physics/hit-3dpoly'
-import type { HitObject } from '../../physics/hit-object'
-import { LineSeg } from '../../physics/line-seg'
-import { logger } from '../../util/logger'
-import type { TableData } from './table-data'
+import { Vertex2D } from '../../math/vertex2d.js'
+import { Vertex3D } from '../../math/vertex3d.js'
+import { Hit3DPoly } from '../../physics/hit-3dpoly.js'
+import type { HitObject } from '../../physics/hit-object.js'
+import { LineSeg } from '../../physics/line-seg.js'
+import { logger } from '../../util/logger.js'
+import type { TableData } from './table-data.js'
 
 export class TableHitGenerator {
 	private readonly data: TableData
