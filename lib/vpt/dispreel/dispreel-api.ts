9b6f60c06b5af454696fb1191783beea0c1509bf
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:22:25 -0400

    refactor(dispreel): add docs to DispReelApi

--- lib/vpt/dispreel/dispreel-api.ts
+++ lib/vpt/dispreel/dispreel-api.ts
@@ -5,7 +5,8 @@ import { MAX_REELS } from '../../physics/constants.js'
 import { ItemApi } from '../item-api.js'
 import type { DispReelData } from './dispreel-data.js'
 
-export class DispReelApi extends ItemApi<DispReelData> {
+export /** DispReelApi. */
+class DispReelApi extends ItemApi<DispReelData> {
 	get BackColor() {
 		return this.data.backColor
 	}
