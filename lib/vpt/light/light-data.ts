649d707493b4d8fb549d74719638d5a33e96d415
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 11:57:57 -0400

    refactor(vpt): simplify light-data — use biff-helper

--- lib/vpt/light/light-data.ts
+++ lib/vpt/light/light-data.ts
@@ -5,6 +5,7 @@ import { BiffParser } from '../../io/biff-parser.js'
 import type { Storage } from '../../io/ole-doc.js'
 import { DragPoint } from '../../math/dragpoint.js'
 import { Vertex2D } from '../../math/vertex2d.js'
+import { handleBiffTag } from '../biff-helper.js'
 import { Enums } from '../enums.js'
 import { ItemData } from '../item-data.js'
 import type { Table } from '../table/table.js'
@@ -113,7 +114,7 @@ export class LightData extends ItemData {
 		return Object.values(table.lights).filter((l) => l.offImage === this.szOffImage).length > 3
 	}
 
-	private async fromTag(buffer: Uint8Array, tag: string, offset: number, len: number): Promise<number> {
+	private async fromTag(buffer: Uint8Array, tag: string, _offset: number, len: number): Promise<number> {
 		if (tag === 'VCEN') {
 			this.center = Vertex2D.get(buffer)
 			return 0
@@ -126,22 +127,15 @@ export class LightData extends ItemData {
 			this.color2 = BiffParser.bgrToRgb(this.getInt(buffer))
 			return 0
 		}
-		if (tag in FLOAT_MAP) {
-			;(this as unknown as Record<string, unknown>)[FLOAT_MAP[tag]] = this.getFloat(buffer)
+		if (
+			handleBiffTag(this as unknown as Record<string, unknown>, this, tag, buffer, len, {
+				float: FLOAT_MAP,
+				int: INT_MAP,
+				bool: BOOL_MAP,
+				string: STRING_MAP,
+			})
+		)
 			return 0
-		}
-		if (tag in INT_MAP) {
-			;(this as unknown as Record<string, unknown>)[INT_MAP[tag]] = this.getInt(buffer)
-			return 0
-		}
-		if (tag in BOOL_MAP) {
-			;(this as unknown as Record<string, unknown>)[BOOL_MAP[tag]] = this.getBool(buffer)
-			return 0
-		}
-		if (tag in STRING_MAP) {
-			;(this as unknown as Record<string, unknown>)[STRING_MAP[tag]] = this.getString(buffer, len)
-			return 0
-		}
 		this.getCommonBlock(buffer, tag, len)
 		return 0
 	}
