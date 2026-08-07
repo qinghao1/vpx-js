31902a7d928d1679f4de875209ba7374cc6848e5
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:31:19 -0400

    refactor(hit-target): fix malformed JSDoc in hit-target-data.ts

--- lib/vpt/hit-target/hit-target-data.ts
+++ lib/vpt/hit-target/hit-target-data.ts
@@ -9,8 +9,8 @@ import { Enums } from '../enums.js'
 import { type IPhysicalData, ItemData } from '../item-data.js'
 import type { Table } from '../table/table.js'
 
-export /** HitTarget data. */
-class HitTargetData extends ItemData implements IPhysicalData {
+/** HitTarget data. */
+export class HitTargetData extends ItemData implements IPhysicalData {
 	public depthBias?: number
 	public disableLightingBelow?: number
 	public disableLightingTop?: number
