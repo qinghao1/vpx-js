2cfc45da5d8c97f0dbe4e0ccda3b0c4d632e5187
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:21:12 -0400

    refactor(light): clean light-state — docs

--- lib/vpt/light/light-state.ts
+++ lib/vpt/light/light-state.ts
@@ -4,7 +4,8 @@
 import { Pool } from '../../util/object-pool.js'
 import { ItemState } from '../item-state.js'
 
-export class LightState extends ItemState {
+export /** Light state. */
+class LightState extends ItemState {
 	public static readonly POOL = new Pool(LightState)
 
 	/**
