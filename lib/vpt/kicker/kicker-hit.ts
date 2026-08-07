c359d12f3287d5ea03d0cdde2e36c5ab66ceb3d4
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:20:59 -0400

    refactor(kicker): clean kicker-hit — docs

--- lib/vpt/kicker/kicker-hit.ts
+++ lib/vpt/kicker/kicker-hit.ts
@@ -18,7 +18,8 @@ import { FLT_MAX } from '../mesh.js'
 import type { Table } from '../table/table.js'
 import type { KickerData } from './kicker-data.js'
 
-export class KickerHit extends HitCircle {
+export /** Kicker hit. */
+class KickerHit extends HitCircle {
 	private data: KickerData
 	public ball?: Ball // The ball inside this kicker
 	public lastCapturedBall?: Ball
