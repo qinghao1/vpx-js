4590e031747719a4ef58f6a39f90ef74809d93ae
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 12:29:20 -0400

    refactor(vpt): tidy kicker-mesh-generator — DRY offsets, concise

--- lib/vpt/kicker/kicker-mesh-generator.ts
+++ lib/vpt/kicker/kicker-mesh-generator.ts
@@ -16,13 +16,9 @@ const kickerSimpleHoleMesh = loadMesh('kicker-simple-hole-mesh')
 const kickerT1Mesh = loadMesh('kicker-t1-mesh')
 const kickerWilliamsMesh = loadMesh('kicker-williams-mesh')
 
-/** Kicke mesh generator. */
+/** Generates kicker mesh. @see https://github.com/vpinball/vpinball/blob/master/kicker.cpp */
 export class KickerMeshGenerator {
-	private readonly data: KickerData
-
-	constructor(data: KickerData) {
-		this.data = data
-	}
+	constructor(private readonly data: KickerData) {}
 
 	public getMesh(table: Table): Mesh {
 		const baseHeight =
@@ -31,59 +27,56 @@ export class KickerMeshGenerator {
 	}
 
 	private generateMesh(table: Table, baseHeight: number): Mesh {
-		let zOffset = 0.0
-		let zRot = this.data.orientation
-		switch (this.data.kickerType) {
-			case Enums.KickerType.KickerCup:
-				zOffset = f4(-0.18)
-				break
-			case Enums.KickerType.KickerWilliams:
-				zRot = f4(this.data.orientation + 90.0)
-				break
-			case Enums.KickerType.KickerHole:
-				zRot = 0.0
-				break
-			case Enums.KickerType.KickerHoleSimple:
-			default:
-				zRot = 0.0
-				break
-		}
-		const fullMatrix = new Matrix3D()
-		fullMatrix.rotateZMatrix(degToRad(zRot))
-
+		const { zOffset, zRot } = this.getOffsets()
+		const m = new Matrix3D().rotateZMatrix(degToRad(zRot))
 		const mesh = this.getBaseMesh()
-		for (const vertex of mesh.vertices) {
-			const vert = Vertex3D.claim(vertex.x, vertex.y, vertex.z + zOffset).multiplyMatrix(fullMatrix)
-			vertex.x = f4(vert.x * this.data.radius) + this.data.center.x
-			vertex.y = f4(vert.y * this.data.radius) + this.data.center.y
-			vertex.z = f4(f4(vert.z * this.data.radius) * table.getScaleZ()) + baseHeight
-
-			const normal = Vertex3D.claim(vertex.nx, vertex.ny, vertex.nz).multiplyMatrixNoTranslate(fullMatrix)
-			vertex.nx = normal.x
-			vertex.ny = normal.y
-			vertex.nz = normal.z
-
-			Vertex3D.release(vert, normal)
+		const r = this.data.radius
+		const cx = this.data.center.x
+		const cy = this.data.center.y
+		const scaleZ = table.getScaleZ()
+		for (const v of mesh.vertices) {
+			const vert = Vertex3D.claim(v.x, v.y, v.z + zOffset).multiplyMatrix(m)
+			v.x = f4(vert.x * r) + cx
+			v.y = f4(vert.y * r) + cy
+			v.z = f4(vert.z * r * scaleZ) + baseHeight
+			const n = Vertex3D.claim(v.nx, v.ny, v.nz).multiplyMatrixNoTranslate(m)
+			v.nx = n.x
+			v.ny = n.y
+			v.nz = n.z
+			Vertex3D.release(vert, n)
 		}
 		return mesh
 	}
 
-	private getBaseMesh(): Mesh {
-		const name = `kicker-${this.data.getName()}`
+	private getOffsets(): { zOffset: number; zRot: number } {
 		switch (this.data.kickerType) {
 			case Enums.KickerType.KickerCup:
-				return kickerCupMesh.clone(name)
+				return { zOffset: f4(-0.18), zRot: this.data.orientation }
 			case Enums.KickerType.KickerWilliams:
-				return kickerWilliamsMesh.clone(name)
-			case Enums.KickerType.KickerGottlieb:
-				return kickerGottliebMesh.clone(name)
-			case Enums.KickerType.KickerCup2:
-				return kickerT1Mesh.clone(name)
+				return { zOffset: 0, zRot: f4(this.data.orientation + 90) }
 			case Enums.KickerType.KickerHole:
-				return kickerHoleMesh.clone(name)
 			case Enums.KickerType.KickerHoleSimple:
 			default:
-				return kickerSimpleHoleMesh.clone(name)
+				return { zOffset: 0, zRot: 0 }
+		}
+	}
+
+	private getBaseMesh(): Mesh {
+		const n = `kicker-${this.data.getName()}`
+		switch (this.data.kickerType) {
+			case Enums.KickerType.KickerCup:
+				return kickerCupMesh.clone(n)
+			case Enums.KickerType.KickerWilliams:
+				return kickerWilliamsMesh.clone(n)
+			case Enums.KickerType.KickerGottlieb:
+				return kickerGottliebMesh.clone(n)
+			case Enums.KickerType.KickerCup2:
+				return kickerT1Mesh.clone(n)
+			case Enums.KickerType.KickerHole:
+				return kickerHoleMesh.clone(n)
+			case Enums.KickerType.KickerHoleSimple:
+			default:
+				return kickerSimpleHoleMesh.clone(n)
 		}
 	}
 }
