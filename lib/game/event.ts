4479246a5e838a8b126d2757afc413e7ccb289a4
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 11:48:24 -0400

    refactor(game): polish event — add cpp reference

--- lib/game/event.ts
+++ lib/game/event.ts
@@ -1,7 +1,7 @@
 // Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
 // Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
-/** Visual Pinball event DISPIDs. */
+/** VP event DISPIDs. @see https://github.com/vpinball/vpinball/blob/master/pin/input.h */
 export enum Event {
 	GameEventsKeyDown = 1000,
 	GameEventsKeyUp = 1001,
