8fe64ee539315febc0ee86c3485b496a6fccfeee
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 06:02:02 -0400

    refactor(kicker): improve generic JSDoc in kicker-api.ts

--- lib/vpt/kicker/kicker-api.ts
+++ lib/vpt/kicker/kicker-api.ts
@@ -12,7 +12,7 @@ import type { KickerData } from './kicker-data.js'
 import type { KickerHit } from './kicker-hit.js'
 import type { KickerState } from './kicker-state.js'
 
-/** KickerApi. */
+/** Kicker API. */
 export class KickerApi extends ItemApi<KickerData> {
 	private readonly state: KickerState
 	private readonly hit: KickerHit
