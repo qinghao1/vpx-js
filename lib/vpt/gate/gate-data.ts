3d519afa66f32a6caafd58a8dc0f0abbf1b31d44
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:19:50 -0400

    refactor(gate): clean gate-data — docs

--- lib/vpt/gate/gate-data.ts
+++ lib/vpt/gate/gate-data.ts
@@ -7,6 +7,7 @@ import { Vertex2D } from '../../math/vertex2d.js'
 import { Enums } from '../enums.js'
 import { ItemData } from '../item-data.js'
 
+/** Gate data. */
 export class GateData extends ItemData {
 	public angleMax: number = Math.PI / 2.0
 	public angleMin: number = 0
