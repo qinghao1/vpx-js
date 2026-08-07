8ee3e1d10bfa458453a4f78a7e1cf3ad4cd5cdc0
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 12:35:56 -0400

    refactor(vpt): tidy bumper-hit — param props, concise

--- lib/vpt/bumper/bumper-hit.ts
+++ lib/vpt/bumper/bumper-hit.ts
@@ -10,40 +10,25 @@ import type { BumperAnimation } from './bumper-animation.js'
 import type { BumperData } from './bumper-data.js'
 import type { BumperState } from './bumper-state.js'
 
-/** Bumper hit shape. */
+/** Bumper hit shape. @see https://github.com/vpinball/vpinball/blob/master/bumper.cpp */
 export class BumperHit extends HitCircle {
-	private readonly data: BumperData
-	private readonly state: BumperState
-	private readonly animation: BumperAnimation
-	private readonly events: EventProxy
-
-	constructor(data: BumperData, state: BumperState, animation: BumperAnimation, events: EventProxy, height: number) {
+	constructor(
+		private readonly data: BumperData,
+		private readonly state: BumperState,
+		private readonly animation: BumperAnimation,
+		private readonly events: EventProxy,
+		height: number,
+	) {
 		super(data.center, data.radius, height, height + data.heightScale)
-		this.data = data
-		this.state = state
-		this.animation = animation
-
-		this.events = events
-		this.isEnabled = this.data.isCollidable
-		this.scatter = this.data.scatter!
+		this.isEnabled = data.isCollidable
+		this.scatter = data.scatter!
 	}
-
-	public collide(coll: CollisionEvent, physics: PlayerPhysics): void {
-		if (!this.isEnabled) {
-			return
-		}
-
-		// needs to be computed before Collide3DWall()
+	public override collide(coll: CollisionEvent, _physics: PlayerPhysics): void {
+		if (!this.isEnabled) return
 		const dot = coll.hitNormal.dot(coll.ball.hit.vel)
-
-		// reflect ball from wall
 		coll.ball.hit.collide3DWall(coll.hitNormal, this.elasticity, this.elasticityFalloff, this.friction, this.scatter)
-
-		// if velocity greater than threshold level
 		if (this.data.hitEvent && dot <= -this.data.threshold) {
-			// add a chunk of velocity to drive ball away
 			coll.ball.hit.vel.addAndRelease(coll.hitNormal.clone(true).multiplyScalar(this.data.force))
-
 			this.animation.hitEvent = true
 			this.animation.ballHitPosition.setAndRelease(coll.ball.state.pos.clone(true))
 			this.events.fireGroupEvent(Event.HitEventsHit)
