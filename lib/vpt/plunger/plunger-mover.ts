dd363f7b87d0a76e38d0bd933c3da4f282e1044a
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 06:05:44 -0400

    refactor(plunger): condense forward travel limit doc

--- lib/vpt/plunger/plunger-mover.ts
+++ lib/vpt/plunger/plunger-mover.ts
@@ -68,38 +68,7 @@ export class PlungerMover implements MoverObject {
 	 */
 	public speed: number = 0.0
 
-	/**
-	 * Forward travel limit.  When we're about to collide with a ball,
-	 * we'll temporarily set this so the collision location.  We set
-	 * this in HitTest(), and use it (and reset it) in the next call
-	 * to UpdateDisplacements().  This is expressed in absolute
-	 * position coordinates, so the default value (which allows full
-	 * forward travel) is m_frameEnd.
-	 *
-	 * The purpose of this limit is to fix buggy behavior that can
-	 * happen when the ball speed after a collision is slower than the
-	 * plunger speed before the collision.  (In past versions, this
-	 * scenario wasn't really possible, because the plunger code just
-	 * gave the ball the same velocity as the plunger at the time of
-	 * collision.  But this was a bit limiting; the physical process
-	 * we're modeling is really a transfer of momentum, not velocity.
-	 * With the addition of the Momentum Transfer property and the new
-	 * accounting for the relative mass ofthe ball, the ball can now
-	 * come out of the collision with a slower speed than the plunger
-	 * had going in.)
-	 *
-	 * The bug that this can trigger is that the fast-moving plunger
-	 * can overtake and shoot past the slow-moving ball.  Left to their
-	 * own devices, the two objects would just keep moving at their
-	 * programmed velocities.  If the plunger velocity is faster than
-	 * the ball after the collision, the plunger can shoot past the
-	 * ball as though it's not there.  The best way I've found to fix
-	 * this is to add this extra limit on the travel distance.  This
-	 * lets us figure where the ball is at the point of collision and
-	 * explicitly prevent the plunger from going past that until the
-	 * next displacement update, when the ball will have been moved
-	 * as well.
-	 */
+	/** Forward travel limit for next displacement (prevents plunger overtaking slow ball). */
 	public travelLimit: number = 0
 
 	/**
