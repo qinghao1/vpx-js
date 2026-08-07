a946eee5f0ae87b4f691fdcf0bc10590d25fd8fb
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:22:55 -0400

    refactor(ramp): add docs to RampState

--- lib/vpt/ramp/ramp-state.ts
+++ lib/vpt/ramp/ramp-state.ts
@@ -4,7 +4,8 @@
 import { Pool } from '../../util/object-pool.js'
 import { ItemState } from '../item-state.js'
 
-export class RampState extends ItemState {
+export /** RampState. */
+class RampState extends ItemState {
 	public static readonly POOL = new Pool(RampState)
 
 	public type?: number
