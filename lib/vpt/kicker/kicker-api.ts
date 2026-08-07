f9789a50e6963a2f75d1f6c2b7128369145c5d09
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 09:13:52 -0400

    refactor(kicker): add @see link in kicker-api.ts

--- lib/vpt/kicker/kicker-api.ts
+++ lib/vpt/kicker/kicker-api.ts
@@ -12,7 +12,9 @@ import type { KickerData } from './kicker-data.js'
 import type { KickerHit } from './kicker-hit.js'
 import type { KickerState } from './kicker-state.js'
 
-/** Kicker API. */
+/** Kicker API.
+ *
+ * @see https://github.com/vpinball/vpinball/blob/master/kicker.cpp */
 export class KickerApi extends ItemApi<KickerData> {
 	private readonly state: KickerState
 	private readonly hit: KickerHit
