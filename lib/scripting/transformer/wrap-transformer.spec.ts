e691416aeb25384d092b7c6c4ac3b1f09356be09
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:04:07 -0400

    refactor: clean lib/scripting/transformer/wrap-transformer.spec.ts — concise dual copyright, tidy imports

--- lib/scripting/transformer/wrap-transformer.spec.ts
+++ lib/scripting/transformer/wrap-transformer.spec.ts
@@ -1,21 +1,5 @@
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+// Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
+// Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
 import * as chai from 'chai'
 import { expect } from 'chai'
@@ -23,13 +7,12 @@ import sinonChai from 'sinon-chai'
 import { ScriptHelper } from '../../../test/script.helper'
 import { ThreeHelper } from '../../../test/three.helper'
 import { NodeBinaryReader } from '../../io/binary-reader.node.js'
-import { Table } from '../../vpt/table/table'
-import { ReferenceTransformer } from './reference-transformer'
-import { WrapTransformer } from './wrap-transformer'
+import { Table } from '../../vpt/table/table.js'
+import { ReferenceTransformer } from './reference-transformer.js'
+import { WrapTransformer } from './wrap-transformer.js'
 
 chai.use((sinonChai as any).default ?? sinonChai)
 
-/* tslint:disable:no-unused-expression */
 describe('The scripting wrap transformer', () => {
 	const three = new ThreeHelper()
 	let table: Table
