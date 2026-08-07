d44f0e13985fc1778564f01a14d4c366c2801b7e
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 01:59:32 -0400

    refactor(math): clean Vertex2D with concise JSDoc

--- lib/math/vertex2d.ts
+++ lib/math/vertex2d.ts
@@ -1,159 +1,133 @@
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+import { Pool } from '../util/object-pool.js'
+import { f4 } from './float.js'
+import type { IRenderVertex, Vertex } from './vertex.js'
 
-/* tslint:disable:variable-name adjacent-overload-signatures */
-import { Pool } from '../util/object-pool'
-import { f4 } from './float'
-import type { IRenderVertex, Vertex } from './vertex'
-
+/** 2D single-precision vector with pooled allocation. */
 export class Vertex2D implements Vertex {
-	public static readonly POOL = new Pool(Vertex2D)
+	static readonly POOL = new Pool(Vertex2D)
 
-	public readonly isVector2 = true
-	public readonly isVector3 = false
+	readonly isVector2 = true as const
+	readonly isVector3 = false as const
 
-	set x(_x: number) {
-		this._x = f4(_x)
-	}
-	set y(_y: number) {
-		this._y = f4(_y)
-	}
+	private _x = 0
+	private _y = 0
 
-	get x() {
+	get x(): number {
 		return this._x
 	}
-	get y() {
+	set x(v: number) {
+		this._x = f4(v)
+	}
+	get y(): number {
 		return this._y
 	}
-
-	private _x!: number
-	private _y!: number
+	set y(v: number) {
+		this._y = f4(v)
+	}
 
 	constructor(x?: number, y?: number) {
-		this.x = x || 0
-		this.y = y || 0
+		this.x = x ?? 0
+		this.y = y ?? 0
 	}
 
-	public static get(buffer: Uint8Array) {
-		const v2 = new Vertex2D()
-		v2.x = new DataView(buffer.buffer, buffer.byteOffset, buffer.byteLength).getFloat32(0, true)
-		v2.y = new DataView(buffer.buffer, buffer.byteOffset, buffer.byteLength).getFloat32(4, true)
-		return v2
+	/** Reads a 2D position from the start of `buffer`. */
+	static get(buffer: Uint8Array): Vertex2D {
+		const view = new DataView(buffer.buffer, buffer.byteOffset, buffer.byteLength)
+		const v = new Vertex2D()
+		v._x = view.getFloat32(0, true)
+		v._y = view.getFloat32(4, true)
+		return v
 	}
 
-	public static claim(x?: number, y?: number): Vertex2D {
-		return Vertex2D.POOL.get().set(x || 0, y || 0)
+	/** Claims a pooled instance. */
+	static claim(x?: number, y?: number): Vertex2D {
+		return Vertex2D.POOL.get().set(x ?? 0, y ?? 0)
 	}
 
-	public static release(...vertices: Vertex2D[]) {
-		for (const vertex of vertices) {
-			Vertex2D.POOL.release(vertex)
-		}
+	static release(...vertices: Vertex2D[]): void {
+		for (const v of vertices) Vertex2D.POOL.release(v)
 	}
 
-	public static reset(v: Vertex2D): void {
+	static reset(v: Vertex2D): void {
 		v.set(0, 0)
 	}
 
-	public set(x: number, y: number): this {
+	set(x: number, y: number): this {
 		this.x = x
 		this.y = y
 		return this
 	}
 
-	public setZero(): this {
+	setZero(): this {
 		return this.set(0, 0)
 	}
 
-	public clone(recycle = false): Vertex2D {
-		if (recycle) {
-			return Vertex2D.POOL.get().set(this._x, this._y)
-		}
-		return new Vertex2D(this._x, this._y)
+	clone(recycle = false): Vertex2D {
+		return recycle ? Vertex2D.POOL.get().set(this._x, this._y) : new Vertex2D(this._x, this._y)
 	}
 
-	public add(v: Vertex2D): this {
+	add(v: Vertex2D): this {
 		this.x += v.x
 		this.y += v.y
 		return this
 	}
 
-	public addAndRelease(v: Vertex2D): this {
+	/** Adds `v` and releases it. */
+	addAndRelease(v: Vertex2D): this {
 		this.add(v)
 		Vertex2D.release(v)
 		return this
 	}
 
-	public sub(v: Vertex2D): this {
+	sub(v: Vertex2D): this {
 		this.x -= v.x
 		this.y -= v.y
 		return this
 	}
 
-	public subAndRelease(v: Vertex2D): this {
+	subAndRelease(v: Vertex2D): this {
 		this.sub(v)
 		Vertex2D.release(v)
 		return this
 	}
 
-	public normalize(): this {
+	normalize(): this {
 		return this.divideScalar(this.length() || 1)
 	}
 
-	public divideScalar(scalar: number): this {
+	divideScalar(scalar: number): this {
 		return this.multiplyScalar(f4(1 / scalar))
 	}
 
-	public multiplyScalar(scalar: number): this {
+	multiplyScalar(scalar: number): this {
 		this.x *= f4(scalar)
 		this.y *= f4(scalar)
 		return this
 	}
 
-	public length(): number {
+	length(): number {
 		return f4(Math.sqrt(f4(f4(this.x * this.x) + f4(this.y * this.y))))
 	}
 
-	public lengthSq() {
+	lengthSq(): number {
 		return this.x * this.x + this.y * this.y
 	}
 
-	public dot(pv: Vertex2D) {
-		return this.x * pv.x + this.y * pv.y
+	dot(v: Vertex2D): number {
+		return this.x * v.x + this.y * v.y
 	}
 
-	public equals(v?: Vertex2D): boolean {
-		if (!v) {
-			return false
-		}
-		return this.x === v.x && this.y === v.y
+	equals(v?: Vertex2D): boolean {
+		return !!v && this.x === v.x && this.y === v.y
 	}
 }
 
+/** 2D vertex with editor flags. */
 export class RenderVertex extends Vertex2D implements IRenderVertex {
-	public fSmooth: boolean = false
-	public fSlingshot: boolean = false
-	public fControlPoint: boolean = false // Whether this point was a control point on the curve
-
+	fSmooth = false
+	fSlingshot = false
+	fControlPoint = false
 	constructor(x?: number, y?: number) {
 		super(x, y)
 	}
 }
-
-//setTimeout(() => Vertex2D.POOL.enableDebug(10000), 20000);
