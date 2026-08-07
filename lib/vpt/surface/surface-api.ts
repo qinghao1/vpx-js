9fbc4ef2cd4ccf79403947d8e0cf52fc7e90e2af
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:23:03 -0400

    refactor(surface): add docs to SurfaceApi

--- lib/vpt/surface/surface-api.ts
+++ lib/vpt/surface/surface-api.ts
@@ -10,7 +10,8 @@ import type { SurfaceData } from './surface-data.js'
 import type { SurfaceHitGenerator } from './surface-hit-generator.js'
 import type { SurfaceState } from './surface-state.js'
 
-export class SurfaceApi extends ItemApi<SurfaceData> {
+export /** SurfaceApi. */
+class SurfaceApi extends ItemApi<SurfaceData> {
 	private readonly hitGenerator: SurfaceHitGenerator
 	private readonly state: SurfaceState
 	private readonly hits: HitObject[]
