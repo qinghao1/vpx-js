aed46f03a2dd88702ff791e96b2d42f6ce964361
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:04:11 -0400

    refactor: clean lib/vpt/table/table-data.ts — concise dual copyright, tidy imports

--- lib/vpt/table/table-data.ts
+++ lib/vpt/table/table-data.ts
@@ -1,30 +1,13 @@
-/* tslint:disable:variable-name */
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+// Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
+// Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
-import { BiffParser } from '../../io/biff-parser'
-import type { Storage } from '../../io/ole-doc'
-import { Vertex2D } from '../../math/vertex2d'
-import type { Vertex3D } from '../../math/vertex3d'
-import { Enums } from '../enums'
-import { ItemData } from '../item-data'
-import { Material, SaveMaterial, SavePhysicsMaterial } from '../material'
+import { BiffParser } from '../../io/biff-parser.js'
+import type { Storage } from '../../io/ole-doc.js'
+import { Vertex2D } from '../../math/vertex2d.js'
+import type { Vertex3D } from '../../math/vertex3d.js'
+import { Enums } from '../enums.js'
+import { ItemData } from '../item-data.js'
+import { Material, SaveMaterial, SavePhysicsMaterial } from '../material.js'
 
 /**
  * Global data about the table.
