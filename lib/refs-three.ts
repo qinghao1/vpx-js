4a2f24a4402d79fbe49ac6a4d09322bc2192aab7
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:26:16 -0400

    refactor(refs): extract shared three re-exports, add explicit patchFileLoader

---
+++ lib/refs-three.ts
@@ -0,0 +1,74 @@
+// Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
+// Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
+
+/** Shared Three.js re-exports. */
+export {
+	AdditiveBlending,
+	AnimationClip,
+	Bone,
+	Box3,
+	BufferAttribute,
+	BufferGeometry,
+	Camera,
+	CanvasTexture,
+	ClampToEdgeWrapping,
+	Color,
+	DataTexture,
+	DataTextureLoader,
+	DefaultLoadingManager,
+	DoubleSide,
+	ExtrudeGeometry,
+	ExtrudeGeometry as ExtrudeBufferGeometry,
+	Float32BufferAttribute,
+	FloatType,
+	Group,
+	HalfFloatType,
+	InterleavedBufferAttribute,
+	InterpolateDiscrete,
+	InterpolateLinear,
+	KeyframeTrack,
+	Light,
+	Line,
+	LinearFilter,
+	LinearMipMapLinearFilter,
+	LinearMipMapNearestFilter,
+	LinearSRGBColorSpace,
+	LoadingManager,
+	Material,
+	MathUtils as Math,
+	Matrix3,
+	Matrix4,
+	Mesh,
+	MeshStandardMaterial,
+	MirroredRepeatWrapping,
+	NearestFilter,
+	NearestMipMapLinearFilter,
+	NearestMipMapNearestFilter,
+	NoColorSpace,
+	Object3D,
+	Path,
+	PointLight,
+	PointLightHelper,
+	PropertyBinding,
+	RepeatWrapping,
+	RGBAFormat,
+	Scene,
+	Shape,
+	SpotLight,
+	SRGBColorSpace,
+	Texture,
+	TextureLoader,
+	TriangleFanDrawMode,
+	TriangleStripDrawMode,
+	UnsignedByteType,
+	Vector2,
+	Vector3,
+} from 'three'
+
+export const RGBFormat = 1022
+export const RGBEFormat = 1023
+export const RGBEEncoding = 3000
+export const LinearEncoding = 3000
+export const sRGBEncoding = 3001
+export const GammaEncoding = 3007
+export type { PixelFormat, TextureDataType } from 'three'
