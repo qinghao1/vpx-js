7cc567cee5ca0eb6358bea31a07d976782fa15c1
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 06:07:37 -0400

    refactor(refs.node.ts): add file doc to refs.node.ts

--- lib/refs.node.ts
+++ lib/refs.node.ts
@@ -1,6 +1,7 @@
 // Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
 // Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
+/** refs.node.ts — Visual Pinball lib module. */
 import { FileLoader } from 'three'
 
 export { exportGltf } from './gltf/export-gltf.node.js'
