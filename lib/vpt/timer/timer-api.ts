249400e0d3fcfaa9f98a3cc08f785aa2702fe0ab
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 13:40:09 -0400

    refactor(timer): tidy timer-api — @see

--- lib/vpt/timer/timer-api.ts
+++ lib/vpt/timer/timer-api.ts
@@ -7,9 +7,7 @@ import { ItemApi } from '../item-api.js'
 import type { Table } from '../table/table.js'
 import type { TimerData } from './timer-data.js'
 
-/** Timer API.
- *
- * @see https://github.com/vpinball/vpinball/blob/master/timer.cpp */
+/** Timer API — VBS surface for `Timer`. @see https://github.com/vpinball/vpinball/blob/master/timer.cpp */
 export class TimerApi extends ItemApi<TimerData> {
 	constructor(data: TimerData, events: EventProxy, player: Player, table: Table) {
 		super(data, events, player, table)
