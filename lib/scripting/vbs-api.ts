b92fab3d18fb9315d6f8bbbee23063aad5efc91d
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:19:18 -0400

    refactor(scripting): clean vbs-api — docs

--- lib/scripting/vbs-api.ts
+++ lib/scripting/vbs-api.ts
@@ -1,6 +1,7 @@
 // Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
 // Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
+/** Base for VBS-exposed APIs. */
 export abstract class VbsApi {
 	private propertyMap?: { [key: string]: string }
 
