adec8afa7a9ec9f184f033bfdd65bea17c1313e0
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:04:06 -0400

    refactor: clean lib/physics/hit-3dpoly.ts — concise dual copyright, tidy imports

--- lib/physics/hit-3dpoly.ts
+++ lib/physics/hit-3dpoly.ts
@@ -1,30 +1,13 @@
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+// Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
+// Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
-/* tslint:disable:no-bitwise */
-import { Event } from '../game/event'
-import { Vertex3D } from '../math/vertex3d'
-import type { Ball } from '../vpt/ball/ball'
-import type { CollisionEvent } from './collision-event'
-import { CollisionType } from './collision-type'
-import { C_CONTACTVEL, C_LOWNORMVEL, PHYS_TOUCH, STATICTIME } from './constants'
-import { HitObject } from './hit-object'
+import { Event } from '../game/event.js'
+import { Vertex3D } from '../math/vertex3d.js'
+import type { Ball } from '../vpt/ball/ball.js'
+import type { CollisionEvent } from './collision-event.js'
+import { CollisionType } from './collision-type.js'
+import { C_CONTACTVEL, C_LOWNORMVEL, PHYS_TOUCH, STATICTIME } from './constants.js'
+import { HitObject } from './hit-object.js'
 
 export class Hit3DPoly extends HitObject {
 	private readonly rgv: Vertex3D[]
