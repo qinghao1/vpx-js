18bc46f586bf8a358c294addd638462fc7eac576
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:23:05 -0400

    refactor(trigger): add docs to TriggerAnimation

--- lib/vpt/trigger/trigger-animation.ts
+++ lib/vpt/trigger/trigger-animation.ts
@@ -8,7 +8,8 @@ import type { Table } from '../table/table.js'
 import type { TriggerData } from './trigger-data.js'
 import type { TriggerState } from './trigger-state.js'
 
-export class TriggerAnimation implements IAnimation {
+export /** TriggerAnimation. */
+class TriggerAnimation implements IAnimation {
 	private readonly data: TriggerData
 	private readonly state: TriggerState
 
