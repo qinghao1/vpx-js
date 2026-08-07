80d52fc946db76756bb154d56d18d2914a4d6432
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:20:29 -0400

    refactor(dispreel): clean dispreel — docs

--- lib/vpt/dispreel/dispreel.ts
+++ lib/vpt/dispreel/dispreel.ts
@@ -10,7 +10,8 @@ import type { Table } from '../table/table.js'
 import { DispReelApi } from './dispreel-api.js'
 import { DispReelData } from './dispreel-data.js'
 
-export class DispReel extends Item<DispReelData> implements IScriptable<DispReelApi> {
+export /** Runtime disp reel. */
+class DispReel extends Item<DispReelData> implements IScriptable<DispReelApi> {
 	private api?: DispReelApi
 
 	public static async fromStorage(storage: Storage, itemName: string): Promise<DispReel> {
