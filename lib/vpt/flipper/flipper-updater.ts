2a5487e79a9a713934e4d60781a1aeadfc2771cb
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 12:33:34 -0400

    refactor(vpt): tidy flipper-updater — param prop, DRY

--- lib/vpt/flipper/flipper-updater.ts
+++ lib/vpt/flipper/flipper-updater.ts
@@ -8,15 +8,14 @@ import type { Table } from '../table/table.js'
 import type { FlipperData } from './flipper-data.js'
 import type { FlipperState } from './flipper-state.js'
 
-/** Syncs flipper state to render node. */
+/** Syncs flipper state to render node. @see https://github.com/vpinball/vpinball/blob/master/flipper.cpp */
 export class FlipperUpdater extends ItemUpdater<FlipperState> {
-	private readonly data: FlipperData
-
-	constructor(data: FlipperData, state: FlipperState) {
+	constructor(
+		private readonly data: FlipperData,
+		state: FlipperState,
+	) {
 		super(state)
-		this.data = data
 	}
-
 	public applyState<NODE, GEOMETRY, POINT_LIGHT>(
 		obj: NODE,
 		state: FlipperState,
@@ -28,24 +27,22 @@ export class FlipperUpdater extends ItemUpdater<FlipperState> {
 		this.applyMaterial(obj, state.material, state.texture, renderApi, table)
 		if (state.center || state.angle !== undefined) this.applyTransformation(obj, renderApi, table)
 	}
-
 	private applyTransformation<NODE, GEOMETRY, POINT_LIGHT>(
 		obj: NODE,
 		renderApi: IRenderApi<NODE, GEOMETRY, POINT_LIGHT>,
 		table: Table,
 	): void {
 		const center = this.state.center ?? this.data.center
-		const diffX = center.x - this.data.center.x
-		const diffY = center.y - this.data.center.y
-		const height =
-			table.getSurfaceHeight(this.data.szSurface, this.data.center.x, this.data.center.y) * table.getScaleZ()
-		const matToOrigin = Matrix3D.claim().setTranslation(-this.data.center.x, -this.data.center.y, height)
-		const matFromOrigin = Matrix3D.claim().setTranslation(this.data.center.x, this.data.center.y, -height)
+		const dx = center.x - this.data.center.x,
+			dy = center.y - this.data.center.y
+		const h = table.getSurfaceHeight(this.data.szSurface, this.data.center.x, this.data.center.y) * table.getScaleZ()
+		const m0 = Matrix3D.claim().setTranslation(-this.data.center.x, -this.data.center.y, h)
+		const m1 = Matrix3D.claim().setTranslation(this.data.center.x, this.data.center.y, -h)
 		const angle = this.state.angle ?? 0
-		const matRotate = Matrix3D.claim().rotateZMatrix(angle - (this.data.startAngle * Math.PI) / 180)
-		const matTrans = Matrix3D.claim().setTranslation(diffX, diffY, 0)
-		const matrix = matToOrigin.multiply(matRotate).multiply(matFromOrigin).multiply(matTrans)
-		renderApi.applyMatrixToNode(matrix, obj)
-		Matrix3D.release(matToOrigin, matFromOrigin, matRotate, matTrans)
+		const mr = Matrix3D.claim().rotateZMatrix(angle - (this.data.startAngle * Math.PI) / 180)
+		const mt = Matrix3D.claim().setTranslation(dx, dy, 0)
+		const m = m0.multiply(mr).multiply(m1).multiply(mt)
+		renderApi.applyMatrixToNode(m, obj)
+		Matrix3D.release(m0, m1, mr, mt)
 	}
 }
