b3a0f98a88e698fc1064fbd123f1e69b0302a61b
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:00:17 -0400

    refactor(math): clean catmull-curve.ts — remove header, tidy imports

--- lib/math/catmull-curve.ts
+++ lib/math/catmull-curve.ts
@@ -1,26 +1,7 @@
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
-import { f4 } from './float'
-import type { IRenderVertex, Vertex } from './vertex'
-import { RenderVertex, type Vertex2D } from './vertex2d'
-import { RenderVertex3D, type Vertex3D } from './vertex3d'
+import { f4 } from './float.js'
+import type { IRenderVertex, Vertex } from './vertex.js'
+import { RenderVertex, type Vertex2D } from './vertex2d.js'
+import { RenderVertex3D, type Vertex3D } from './vertex3d.js'
 
 /**
  * VPinball's implementation of the Catmull Curve.
