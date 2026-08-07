e9b766b6841bfda39dba24872990af740d19a319
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:30:02 -0400

    refactor(gltf): add file doc to gltf types

--- lib/gltf/gltf.ts
+++ lib/gltf/gltf.ts
@@ -1,6 +1,7 @@
 // Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
 // Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
+/** glTF 2.0 type definitions. */
 export type GltfId = number
 
 interface GltfNamedElement extends GltfElement {
