1519b5c405c3be16fe22556051d7bb3eaba7b91c
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 12:10:14 -0400

    refactor(vpt): tidy ramp-state — concise docs

--- lib/vpt/ramp/ramp-state.ts
+++ lib/vpt/ramp/ramp-state.ts
@@ -5,7 +5,6 @@ import { Pool } from '../../util/object-pool.js'
 import { ItemState } from '../item-state.js'
 
 /** Ramp state.
- *
  * @see https://github.com/vpinball/vpinball/blob/master/ramp.cpp */
 export class RampState extends ItemState {
 	public static readonly POOL = new Pool(RampState)
@@ -145,7 +144,6 @@ export class RampState extends ItemState {
 	}
 
 	public equals(state: RampState): boolean {
-		/* istanbul ignore if: we don't actually pass empty states. */
 		if (!state) {
 			return false
 		}
