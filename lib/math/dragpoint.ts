799f999018c9902699fbae75fddf5819154a8967
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:00:17 -0400

    refactor(math): clean dragpoint.ts — remove header, tidy imports

--- lib/math/dragpoint.ts
+++ lib/math/dragpoint.ts
@@ -1,28 +1,9 @@
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
-import { BiffParser } from '../io/biff-parser'
-import type { CatmullCurve } from './catmull-curve'
-import { f4 } from './float'
-import type { IRenderVertex, Vertex } from './vertex'
-import type { RenderVertex } from './vertex2d'
-import { Vertex3D } from './vertex3d'
+import { BiffParser } from '../io/biff-parser.js'
+import type { CatmullCurve } from './catmull-curve.js'
+import { f4 } from './float.js'
+import type { IRenderVertex, Vertex } from './vertex.js'
+import type { RenderVertex } from './vertex2d.js'
+import { Vertex3D } from './vertex3d.js'
 
 export const HIT_SHAPE_DETAIL_LEVEL = 7.0
 
