ac570105eaf9520ce96b27947745e8fbde17f085
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:04:07 -0400

    refactor: clean lib/scripting/stdlib/math.spec.ts — concise dual copyright, tidy imports

--- lib/scripting/stdlib/math.spec.ts
+++ lib/scripting/stdlib/math.spec.ts
@@ -1,33 +1,16 @@
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+// Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
+// Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
 import * as chai from 'chai'
 import { expect } from 'chai'
 import sinonChai from 'sinon-chai'
-import { TableBuilder } from '../../../test/table-builder'
-import { Player } from '../../game/player'
-import type { Table } from '../../vpt/table/table'
-import { Transpiler } from '../transpiler'
+import { TableBuilder } from '../../../test/table-builder.js'
+import { Player } from '../../game/player.js'
+import type { Table } from '../../vpt/table/table.js'
+import { Transpiler } from '../transpiler.js'
 
 chai.use((sinonChai as any).default ?? sinonChai)
 
-/* tslint:disable:no-unused-expression */
 describe('The VBScript math stdlib', () => {
 	let table: Table
 	let player: Player
