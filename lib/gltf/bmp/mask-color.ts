7208d6dcd6e7b0058b89184c35780900e3e02d15
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:04:05 -0400

    refactor: clean lib/gltf/bmp/mask-color.ts — concise dual copyright, tidy imports

--- lib/gltf/bmp/mask-color.ts
+++ lib/gltf/bmp/mask-color.ts
@@ -1,4 +1,6 @@
-/* tslint:disable:no-bitwise */
+// Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
+// Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
+
 // We have these:
 //
 // const sample = 0101 0101 0101 0101
