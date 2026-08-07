bc5f9bf11899c748dbe7a4885d94e9ab3d817164
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 09:49:43 -0400

    refactor(hit-target): simplify api — remove redundant docs

--- lib/vpt/hit-target/hit-target-api.ts
+++ lib/vpt/hit-target/hit-target-api.ts
@@ -36,7 +36,6 @@ export class HitTargetApi extends ItemApi<HitTargetData> {
 		this.animation = animation
 	}
 
-	/** Get Image. */
 	get Image() {
 		return this.state.texture
 	}
@@ -44,200 +43,171 @@ export class HitTargetApi extends ItemApi<HitTargetData> {
 		this._assertNonHdrImage(v)
 		this.state.texture = v
 	}
-	/** Get Material. */
 	get Material() {
 		return this.state.material
 	}
 	set Material(v) {
 		this.state.material = v
 	}
-	/** Get Visible. */
 	get Visible() {
 		return this.state.isVisible
 	}
 	set Visible(v) {
 		this.state.isVisible = v
 	}
-	/** Get X. */
 	get X() {
 		return this.data.position.x
 	}
 	set X(v) {
 		this.data.position.x = v
 	}
-	/** Get Y. */
 	get Y() {
 		return this.data.position.y
 	}
 	set Y(v) {
 		this.data.position.y = v
 	}
-	/** Get Z. */
 	get Z() {
 		return this.data.position.z
 	}
 	set Z(v) {
 		this.data.position.z = v
 	}
-	/** Get ScaleX. */
 	get ScaleX() {
 		return this.data.vSize.x
 	}
 	set ScaleX(v) {
 		this.data.vSize.x = v
 	}
-	/** Get ScaleY. */
 	get ScaleY() {
 		return this.data.vSize.y
 	}
 	set ScaleY(v) {
 		this.data.vSize.y = v
 	}
-	/** Get ScaleZ. */
 	get ScaleZ() {
 		return this.data.vSize.z
 	}
 	set ScaleZ(v) {
 		this.data.vSize.z = v
 	}
-	/** Get Orientation. */
 	get Orientation() {
 		return this.data.rotZ
 	}
 	set Orientation(v) {
 		this.data.rotZ = v
 	}
-	/** Get HasHitEvent. */
 	get HasHitEvent() {
 		return this.data.useHitEvent
 	}
 	set HasHitEvent(v) {
 		this.data.useHitEvent = v
 	}
-	/** Get Threshold. */
 	get Threshold() {
 		return this.data.threshold
 	}
 	set Threshold(v) {
 		this.data.threshold = v
 	}
-	/** Get Elasticity. */
 	get Elasticity() {
 		return this.data.elasticity
 	}
 	set Elasticity(v) {
 		this.data.elasticity = v
 	}
-	/** Get ElasticityFalloff. */
 	get ElasticityFalloff() {
 		return this.data.elasticityFalloff
 	}
 	set ElasticityFalloff(v) {
 		this.data.elasticityFalloff = v
 	}
-	/** Get Friction. */
 	get Friction() {
 		return this.data.friction
 	}
 	set Friction(v) {
 		this.data.friction = clamp(v, 0, 1)
 	}
-	/** Get Scatter. */
 	get Scatter() {
 		return this.data.scatter
 	}
 	set Scatter(v) {
 		this.data.scatter = v
 	}
-	/** Get Collidable. */
 	get Collidable() {
 		return this.data.isCollidable
 	}
 	set Collidable(v) {
 		this._setCollidable(v)
 	}
-	/** Get DisableLighting. */
 	get DisableLighting() {
 		return !!this.data.disableLightingTop
 	}
 	set DisableLighting(v) {
 		this.data.disableLightingTop = v ? 1 : 0
 	}
-	/** Get BlendDisableLighting. */
 	get BlendDisableLighting() {
 		return this.data.disableLightingTop
 	}
 	set BlendDisableLighting(v) {
 		this.data.disableLightingTop = v
 	}
-	/** Get BlendDisableLightingFromBelow. */
 	get BlendDisableLightingFromBelow() {
 		return this.data.disableLightingBelow
 	}
 	set BlendDisableLightingFromBelow(v) {
 		this.data.disableLightingBelow = v
 	}
-	/** Get ReflectionEnabled. */
 	get ReflectionEnabled() {
 		return this.data.isReflectionEnabled
 	}
 	set ReflectionEnabled(v) {
 		this.data.isReflectionEnabled = v
 	}
-	/** Get DropSpeed. */
 	get DropSpeed() {
 		return this.data.dropSpeed
 	}
 	set DropSpeed(v) {
 		this.data.dropSpeed = v
 	}
-	/** Get IsDropped. */
 	get IsDropped() {
 		return this.data.isDropped
 	}
 	set IsDropped(v) {
 		this._setDropped(v, this.table, this.player.getPhysics())
 	}
-	/** Get LegacyMode. */
 	get LegacyMode() {
 		return this.data.legacy
 	}
 	set LegacyMode(v) {
 		this.data.legacy = v
 	}
-	/** Get DrawStyle. */
 	get DrawStyle() {
 		return this.data.targetType
 	}
 	set DrawStyle(v) {
 		this.data.targetType = v
 	}
-	/** Get PhysicsMaterial. */
 	get PhysicsMaterial() {
 		return this.data.szPhysicsMaterial
 	}
 	set PhysicsMaterial(v) {
 		this.data.szPhysicsMaterial = v
 	}
-	/** Get OverwritePhysics. */
 	get OverwritePhysics() {
 		return this.data.overwritePhysics
 	}
 	set OverwritePhysics(v) {
 		this.data.overwritePhysics = v
 	}
-	/** Get DepthBias. */
 	get DepthBias() {
 		return this.data.depthBias
 	}
 	set DepthBias(v) {
 		this.data.depthBias = v
 	}
-	/** Get HitThreshold. */
 	get HitThreshold() {
 		return this.events.currentHitThreshold
 	}
-	/** Get RaiseDelay. */
 	get RaiseDelay() {
 		return this.data.raiseDelay
 	}
