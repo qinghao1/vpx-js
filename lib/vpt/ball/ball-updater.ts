c94fb40525caabc27cb15413d18d85d091c2d945
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 12:33:20 -0400

    refactor(vpt): tidy ball-updater — concise matrix, param prop

--- lib/vpt/ball/ball-updater.ts
+++ lib/vpt/ball/ball-updater.ts
@@ -8,53 +8,50 @@ import type { Table } from '../table/table.js'
 import type { BallData } from './ball-data.js'
 import type { BallState } from './ball-state.js'
 
-/** Updates ball render state. */
+/** Syncs ball state to render node. @see https://github.com/vpinball/vpinball/blob/master/ball.cpp */
 export class BallUpdater extends ItemUpdater<BallState> {
 	private readonly data: BallData
-
 	constructor(state: BallState, data: BallData) {
 		super(state)
 		this.data = data
 	}
-
 	public applyState<NODE, GEOMETRY, POINT_LIGHT>(
 		obj: NODE,
 		state: BallState,
 		renderApi: IRenderApi<NODE, GEOMETRY, POINT_LIGHT>,
-		table: Table,
+		_table: Table,
 	): void {
 		Object.assign(this.state, state)
-
-		const pos = this.state.pos
-		const zHeight = !this.state.isFrozen ? pos.z : pos.z - this.data.radius
-		const orientation = Matrix3D.claim().setEach(
-			this.state.orientation.matrix[0][0],
-			this.state.orientation.matrix[1][0],
-			this.state.orientation.matrix[2][0],
-			0.0,
-			this.state.orientation.matrix[0][1],
-			this.state.orientation.matrix[1][1],
-			this.state.orientation.matrix[2][1],
-			0.0,
-			this.state.orientation.matrix[0][2],
-			this.state.orientation.matrix[1][2],
-			this.state.orientation.matrix[2][2],
-			0.0,
+		const pos = this.state.pos,
+			z = this.state.isFrozen ? pos.z - this.data.radius : pos.z
+		const o = this.state.orientation.matrix
+		const orient = Matrix3D.claim().setEach(
+			o[0]![0]!,
+			o[1]![0]!,
+			o[2]![0]!,
+			0,
+			o[0]![1]!,
+			o[1]![1]!,
+			o[2]![1]!,
+			0,
+			o[0]![2]!,
+			o[1]![2]!,
+			o[2]![2]!,
+			0,
 			0,
 			0,
 			0,
 			1,
 		)
-		const trans = Matrix3D.claim().setTranslation(pos.x, pos.y, zHeight)
-		const matrix = Matrix3D.claim()
+		const trans = Matrix3D.claim().setTranslation(pos.x, pos.y, z)
+		const m = Matrix3D.claim()
 			.setScaling(this.data.radius, this.data.radius, this.data.radius)
-			.preMultiply(orientation)
+			.preMultiply(orient)
 			.multiply(trans)
 			.toRightHanded()
-
-		renderApi.applyMatrixToNode(matrix, obj)
-		Matrix3D.release(orientation, trans, matrix)
-		const maybeObj = obj as unknown as { updateMatrixWorld?: (force: boolean) => void }
-		maybeObj.updateMatrixWorld?.(true)
+		renderApi.applyMatrixToNode(m, obj)
+		Matrix3D.release(orient, trans, m)
+		const maybe = obj as unknown as { updateMatrixWorld?: (f: boolean) => void }
+		maybe.updateMatrixWorld?.(true)
 	}
 }
