d704061cf78568f6810c5b25f6f14aed866a7233
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:24:41 -0400

    refactor(io): clean storage.node — concise docs

--- lib/io/storage.node.ts
+++ lib/io/storage.node.ts
@@ -1,15 +1,14 @@
 // Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
 // Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
+/** In-memory storage for Node. */
 class StorageNode {
-	private readonly storage: Map<string, any> = new Map<string, any>()
-
+	private readonly map = new Map<string, any>()
 	public setItem(name: string, value: any): void {
-		this.storage.set(name, value)
+		this.map.set(name, value)
 	}
-
 	public getItem(name: string): any {
-		return this.storage.get(name)
+		return this.map.get(name)
 	}
 }
 
