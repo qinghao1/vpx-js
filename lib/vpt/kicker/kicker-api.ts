33576ba884b00b65454cacb025ea54ae89e3c3da
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:31:22 -0400

    refactor(kicker): fix malformed JSDoc in kicker-api.ts

--- lib/vpt/kicker/kicker-api.ts
+++ lib/vpt/kicker/kicker-api.ts
@@ -12,8 +12,8 @@ import type { KickerData } from './kicker-data.js'
 import type { KickerHit } from './kicker-hit.js'
 import type { KickerState } from './kicker-state.js'
 
-export /** KickerApi. */
-class KickerApi extends ItemApi<KickerData> {
+/** KickerApi. */
+export class KickerApi extends ItemApi<KickerData> {
 	private readonly state: KickerState
 	private readonly hit: KickerHit
 	private readonly ballCreator: IBallCreationPosition
