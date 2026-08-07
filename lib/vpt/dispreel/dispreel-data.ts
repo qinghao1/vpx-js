d7c154aa5ca3b618de191b44f15a546140e2f60c
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 03:20:29 -0400

    refactor(dispreel): clean dispreel-data — docs

--- lib/vpt/dispreel/dispreel-data.ts
+++ lib/vpt/dispreel/dispreel-data.ts
@@ -6,7 +6,8 @@ import type { Storage } from '../../io/ole-doc.js'
 import { Vertex2D } from '../../math/vertex2d.js'
 import { ItemData } from '../item-data.js'
 
-export class DispReelData extends ItemData {
+export /** DispReel data. */
+class DispReelData extends ItemData {
 	public v1!: Vertex2D
 	public v2!: Vertex2D
 	public width: number = 30.0
