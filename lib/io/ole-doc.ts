f3c0167ac4273a06c71773833c7607b299784f12
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 06:07:37 -0400

    refactor(ole-doc.ts): add file doc to ole-doc.ts

--- lib/io/ole-doc.ts
+++ lib/io/ole-doc.ts
@@ -1,6 +1,7 @@
 // Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
 // Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
+/** ole-doc.ts — Visual Pinball io module. */
 import { EventEmitter } from 'events'
 import { concatUint8Arrays, getDataView } from './binary-helpers.js'
 import { readableStream } from './event-stream.js'
