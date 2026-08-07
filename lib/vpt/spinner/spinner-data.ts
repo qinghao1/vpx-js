0494808d270e96efacd61b30f383d4486dd25f6f
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 09:12:15 -0400

    refactor(spinner): remove grouping comment in spinner-data.ts

--- lib/vpt/spinner/spinner-data.ts
+++ lib/vpt/spinner/spinner-data.ts
@@ -8,7 +8,6 @@ import { ItemData } from '../item-data.js'
 
 /** Spinner data. */
 export class SpinnerData extends ItemData {
-	// appearance, physics, and behavior
 	public center!: Vertex2D
 	public rotation: number = 0
 	public szMaterial?: string
