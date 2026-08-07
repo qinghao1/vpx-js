ac16e2062b4a568a5587c202f2d6b3a24e1cd323
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:04:05 -0400

    refactor: clean lib/io/biff-parser.ts — concise dual copyright, tidy imports

--- lib/io/biff-parser.ts
+++ lib/io/biff-parser.ts
@@ -1,21 +1,5 @@
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+// Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
+// Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
 import { inflate } from 'zlib'
 import { f4 } from '../math/float.js'
@@ -130,11 +114,9 @@ export class BiffParser {
 	}
 
 	public static bgrToRgb(bgr: number) {
-		/* tslint:disable:no-bitwise */
 		const r = (bgr & 0xff) << 16
 		const g = bgr & 0xff00
 		const b = (bgr & 0xff0000) >> 16
-		/* tslint:enable:no-bitwise */
 		return r + g + b
 	}
 
