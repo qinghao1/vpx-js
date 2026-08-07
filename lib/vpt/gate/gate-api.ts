ec0227171f78e833175d15c14945b895caf5a320
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 13:10:24 -0400

    refactor(gate): tidy gate-api — param props, DRY openGate/angle clamp

--- lib/vpt/gate/gate-api.ts
+++ lib/vpt/gate/gate-api.ts
@@ -14,30 +14,19 @@ import type { GateHit } from './gate-hit.js'
 import type { GateMover } from './gate-mover.js'
 import type { GateState } from './gate-state.js'
 
-/** Gate API.
- *
- * @see https://github.com/vpinball/vpinball/blob/master/gate.cpp */
+/** Gate API — VBS surface for `Gate`. @see https://github.com/vpinball/vpinball/blob/master/gate.cpp */
 export class GateApi extends ItemApi<GateData> {
-	private readonly state: GateState
-	private readonly mover: GateMover
-	private readonly hitGate: GateHit
-	private readonly hitLine: LineSeg | null
-
 	constructor(
 		data: GateData,
 		events: EventProxy,
-		state: GateState,
-		mover: GateMover,
-		hitGate: GateHit,
-		hitLine: LineSeg | null,
+		private readonly state: GateState,
+		private readonly mover: GateMover,
+		private readonly hitGate: GateHit,
+		private readonly hitLine: LineSeg | null,
 		player: Player,
 		table: Table,
 	) {
 		super(data, events, player, table)
-		this.state = state
-		this.mover = mover
-		this.hitGate = hitGate
-		this.hitLine = hitLine
 	}
 
 	get Length() {
@@ -122,13 +111,13 @@ export class GateApi extends ItemApi<GateData> {
 		return this.mover.friction
 	}
 	set Friction(v) {
-		this.mover.friction = clamp(v, 0.0, 1.0)
+		this.mover.friction = clamp(v, 0, 1)
 	}
 	get Damping() {
-		return this.mover.damping ** (1.0 / PHYS_FACTOR)
+		return this.mover.damping ** (1 / PHYS_FACTOR)
 	}
 	set Damping(v) {
-		this.mover.damping = this.mover.damping = clamp(v, 0, 1) ** PHYS_FACTOR
+		this.mover.damping = clamp(v, 0, 1) ** PHYS_FACTOR
 	}
 	get GravityFactor() {
 		return this.mover.gravityFactor
@@ -168,132 +157,64 @@ export class GateApi extends ItemApi<GateData> {
 		this.mover.angleMax = this.data.angleMax
 		this.mover.angleMin = this.data.angleMin
 		this.mover.forcedMove = true
-
-		if (isOpen) {
-			this.mover.open = true
-			this.hitGate.isEnabled = false
-			if (this.hitLine) {
-				this.hitLine.isEnabled = false
-			}
-
-			if (this.state.angle < this.mover.angleMax) {
-				this.mover.angleSpeed = 0.2
-			}
-		} else {
-			this.mover.open = false
-			this.hitGate.isEnabled = this.data.isCollidable
-			if (this.hitLine) {
-				this.hitLine.isEnabled = this.data.isCollidable
-			}
-
-			if (this.state.angle > this.mover.angleMin) {
-				this.mover.angleSpeed = -0.2
-			}
-		}
+		this.mover.open = isOpen
+		this.hitGate.isEnabled = isOpen ? false : this.data.isCollidable
+		if (this.hitLine) this.hitLine.isEnabled = this.hitGate.isEnabled
+		if (isOpen && this.state.angle < this.mover.angleMax) this.mover.angleSpeed = 0.2
+		if (!isOpen && this.state.angle > this.mover.angleMin) this.mover.angleSpeed = -0.2
 	}
 
-	private setCloseAngle(angleDeg: number): void {
-		if (this.data.isCollidable) {
-			throw new Error("Gate is collidable! closing angles other than 0 aren't possible!")
-		}
-		let angle = degToRad(angleDeg)
-
-		if (angle > this.data.angleMax) {
-			angle = this.data.angleMax
-		} else if (angle < this.data.angleMin) {
-			angle = this.data.angleMin
-		}
-
-		if (this.mover.angleMax > angle) {
-			// max is bigger
-			this.mover.angleMin = angle
-		} else {
-			this.mover.angleMax = angle // else set new max
-		}
+	private setCloseAngle(deg: number): void {
+		if (this.data.isCollidable) throw new Error("Gate is collidable! closing angles other than 0 aren't possible!")
+		const a = clamp(degToRad(deg), this.data.angleMin, this.data.angleMax)
+		if (this.mover.angleMax > a) this.mover.angleMin = a
+		else this.mover.angleMax = a
 	}
 
-	private setOpenAngle(angleDeg: number): void {
-		if (this.data.isCollidable) {
-			throw new Error("Gate is collidable! open angles other than 90 aren't possible!")
-		}
-		let angle = degToRad(angleDeg)
-
-		if (angle > this.data.angleMax) {
-			angle = this.data.angleMax
-		} else if (angle < this.data.angleMin) {
-			angle = this.data.angleMin
-		}
-
-		if (this.mover.angleMin < angle) {
-			// min is smaller
-			this.mover.angleMax = angle
-		} else {
-			this.mover.angleMin = angle // else set new min
-		}
+	private setOpenAngle(deg: number): void {
+		if (this.data.isCollidable) throw new Error("Gate is collidable! open angles other than 90 aren't possible!")
+		const a = clamp(degToRad(deg), this.data.angleMin, this.data.angleMax)
+		if (this.mover.angleMin < a) this.mover.angleMax = a
+		else this.mover.angleMin = a
 	}
 
-	private setCollidable(isCollidable: boolean): void {
-		this.data.isCollidable = isCollidable
-		this.hitGate.isEnabled = isCollidable
-		if (this.hitLine) {
-			this.hitLine.isEnabled = isCollidable
-		}
+	private setCollidable(v: boolean): void {
+		this.data.isCollidable = v
+		this.hitGate.isEnabled = v
+		if (this.hitLine) this.hitLine.isEnabled = v
 		this.mover.angleMax = this.data.angleMax
 		this.mover.angleMin = this.data.angleMin
-
-		if (isCollidable) {
-			this.mover.angleMin = 0
-		}
+		if (v) this.mover.angleMin = 0
 	}
 
-	public move(dir: number, speed: number, angle: number) {
+	public move(dir: number, speed: number, angle: number): void {
 		this.mover.forcedMove = true
-		this.mover.open = true // move always turns off natural swing
-		this.hitGate.isEnabled = false // and collidable off
-		if (this.hitLine) {
-			this.hitLine.isEnabled = false
-		}
-
-		if (speed <= 0.0) {
-			speed = 0.2
-		} else {
-			speed *= Math.PI / 180.0 // convert to radians
-		}
+		this.mover.open = true
+		this.hitGate.isEnabled = false
+		if (this.hitLine) this.hitLine.isEnabled = false
+		if (speed <= 0) speed = 0.2
+		else speed *= Math.PI / 180
 
 		if (!dir || angle !== 0) {
-			angle *= Math.PI / 180.0 // convert to radians
-
-			if (angle < this.data.angleMin) {
-				angle = this.data.angleMin
-			} else if (angle > this.data.angleMax) {
-				angle = this.data.angleMax
-			}
-
-			const da = angle - this.state.angle // calc true direction
-
-			if (da > 1.0e-5) {
-				dir = +1
-			} else if (da < -1.0e-5) {
-				dir = -1
-			} else {
-				dir = 0 // do nothing
-				this.mover.angleSpeed = 0 // stop
+			angle *= Math.PI / 180
+			angle = clamp(angle, this.data.angleMin, this.data.angleMax)
+			const da = angle - this.state.angle
+			if (da > 1e-5) dir = 1
+			else if (da < -1e-5) dir = -1
+			else {
+				dir = 0
+				this.mover.angleSpeed = 0
 			}
 		} else {
-			angle = dir < 0 ? this.data.angleMin : this.data.angleMax // dir selected and angle not specified
+			angle = dir < 0 ? this.data.angleMin : this.data.angleMax
 		}
 
 		if (dir > 0) {
 			this.mover.angleMax = angle
-
-			if (this.state.angle < this.mover.angleMax) {
-				this.mover.angleSpeed = speed
-			}
+			if (this.state.angle < this.mover.angleMax) this.mover.angleSpeed = speed
 		} else if (dir < 0) {
 			this.mover.angleMin = angle
-			if (this.state.angle > this.mover.angleMin) {
-				this.mover.angleSpeed = -speed
-			}
+			if (this.state.angle > this.mover.angleMin) this.mover.angleSpeed = -speed
 		}
 	}
 
