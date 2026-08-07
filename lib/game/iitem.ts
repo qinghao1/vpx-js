f388454c13a12d6984e2c80602eb9c008b357e81
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:18:47 -0400

    refactor(game): clean iitem — concise docs

--- lib/game/iitem.ts
+++ lib/game/iitem.ts
@@ -1,6 +1,7 @@
 // Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
 // Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
+/** Named table item. */
 export interface IItem {
 	getName(): string
 }
