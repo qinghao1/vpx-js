60acfc2b975d74cc9fa68f1ffe6b3c4963087f83
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 06:07:36 -0400

    refactor(refs.browser.ts): add file doc to refs.browser.ts

--- lib/refs.browser.ts
+++ lib/refs.browser.ts
@@ -1,6 +1,7 @@
 // Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
 // Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
+/** refs.browser.ts — Visual Pinball lib module. */
 export { exportGltf } from './gltf/export-gltf.browser.js'
 export { BrowserBinaryReader as BinaryReader } from './io/binary-reader.browser.js'
 export { storage } from './io/storage.browser.js'
