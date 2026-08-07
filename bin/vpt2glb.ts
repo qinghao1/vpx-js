599fc0c9ae113dd11366a0d57ac1765de198d80a
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 02:04:05 -0400

    refactor: clean bin/vpt2glb.ts — concise dual copyright, tidy imports

--- bin/vpt2glb.ts
+++ bin/vpt2glb.ts
@@ -1,32 +1,15 @@
 #!/usr/bin/env node
-/*
- * VPDB - Virtual Pinball Database
- * Copyright (C) 2019 freezy <freezy@vpdb.io>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+// Copyright (C) 2019 freezy <freezy@vpdb.io> — GPL-2.0 — see LICENSE
+// Copyright (C) 2026 Chu Qinghao <6337103+qinghao1@users.noreply.github.com> — GPL-2.0 — see LICENSE
 
 import { existsSync, writeFileSync } from 'fs'
 import { basename, dirname, resolve } from 'path'
 import { NodeBinaryReader } from '../lib/io/binary-reader.node.js'
-import { ThreeTextureLoaderNode } from '../lib/render/threejs/three-texture-loader-node'
-import { Logger } from '../lib/util/logger'
-import { Table } from '../lib/vpt/table/table'
-import { TableExporter } from '../lib/vpt/table/table-exporter'
+import { ThreeTextureLoaderNode } from '../lib/render/threejs/three-texture-loader-node.js'
+import { Logger } from '../lib/util/logger.js'
+import { Table } from '../lib/vpt/table/table.js'
+import { TableExporter } from '../lib/vpt/table/table-exporter.js'
 
-/* tslint:disable:no-empty no-console */
 ;(async () => {
 	try {
 		const argSrc = process.argv[2]
