9fc3f130ca77fa8638a8591c318b9ee45956f1e1
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 06:08:38 -0400

    refactor(lightseq): add getter docs in lightseq-api.ts

--- lib/vpt/lightseq/lightseq-api.ts
+++ lib/vpt/lightseq/lightseq-api.ts
@@ -6,24 +6,28 @@ import type { LightSeqData } from './lightseq-data.js'
 
 /** LightSeqApi. */
 export class LightSeqApi extends ItemApi<LightSeqData> {
+	/** Get Collection. */
 	get Collection() {
 		return this.data.collection
 	}
 	set Collection(v) {
 		this.data.collection = v
 	}
+	/** Get CenterX. */
 	get CenterX() {
 		return this.data.center.x
 	}
 	set CenterX(v) {
 		this.data.center.x = v
 	}
+	/** Get CenterY. */
 	get CenterY() {
 		return this.data.center.y
 	}
 	set CenterY(v) {
 		this.data.center.y = v
 	}
+	/** Get UpdateInterval. */
 	get UpdateInterval() {
 		return this.data.updateInterval
 	}
