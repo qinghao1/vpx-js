baa6c19cdadb9b0b90b306f50cd613a7b36a3ad1
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2026-08-07 09:12:09 -0400

    refactor(kicker): tidy kicker-data.ts — grouping comment

--- lib/vpt/kicker/kicker-data.ts
+++ lib/vpt/kicker/kicker-data.ts
@@ -9,6 +9,7 @@ import { ItemData } from '../item-data.js'
 
 /** Kicker data. */
 export class KickerData extends ItemData {
+	// appearance, physics, and behavior
 	public kickerType: number = Enums.KickerType.KickerHole
 	public center!: Vertex2D
 	public radius: number = 25
